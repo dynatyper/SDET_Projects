3585ffbd9f8401ae2781653ca828a9d647619762
Kevin Dement <kevin_dement@fanniemae.com>
2019-10-17 10:59:44 -0400

    Create Runner for feature

---
+++ cucumber-works/src/test/java/com/fannie/tests/TestRunner.java
@@ -0,0 +1,17 @@
+package com.fannie.tests;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+			features = {"src/main/resources/com/fannie/features"},
+			glue = {"com.fannie.steps"},
+			plugin = {"pretty"},
+			monochrome = true
+		)
+public class TestRunner {
+	//No Business Logic
+}
